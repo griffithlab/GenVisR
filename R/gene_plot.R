317f0330f099bf58d1c623624a826451ffe35987
Zach Skidmore <zskidmor@genome.wustl.edu>
2015-05-07 16:18:51 -0500

    added code to perform mapping from original to modified log space

--- R/gene_plot.R
+++ R/gene_plot.R
@@ -23,15 +23,13 @@ gene_plot <- function(txdb, gr, genome, reduce=FALSE, transformIntronic=FALSE)
   gene_features <- mapply(rbind, cds, threeUTR, fiveUTR, SIMPLIFY=FALSE)
   gene_features <- lapply(gene_features, na.omit)
   
-  #!!!!!!!!!! Test to  grab intronic regions and transform intronic space
+  # Create a master table based on an intronic log transform then use the master table as a map for mapping coordinates to transformed space
   if(transformIntronic == TRUE)
   {
-#     introns <- formatIntronic(txdb, gr)
-#     gene_features <- format_intronic_space(introns, gene_features, gr)
+    message("transforming intronic space")
     master <- mergeRegions(gene_features, gr)
+    gene_features <- lapply(gene_features, map_coord_space, master=master)
   }
-  
-  return(gene_features)
   
   #!!!!!!!!!!!!! End of the dragons
   
@@ -42,11 +40,23 @@ gene_plot <- function(txdb, gr, genome, reduce=FALSE, transformIntronic=FALSE)
     gene_features[[i]]$Upper <- gene_features[[i]]$Upper + increment
     gene_features[[i]]$Lower <- gene_features[[i]]$Lower + increment
     gene_features[[i]]$Mid <- gene_features[[i]]$Mid + increment
+    if(transformIntronic == TRUE)
+    {
+      gene_features[[i]]$trans_segStart <- min(gene_features[[i]]$trans_start_vec)
+      gene_features[[i]]$trans_segEnd <- max(gene_features[[i]]$trans_end_vec)
+    }
     increment <- increment + 2.2
   }	
   
   # Convert the list object of gene_features into a data frame
   gene_features <- as.data.frame(do.call("rbind", gene_features))
+  
+  # If it is requested to transform intronic, dump the original values in the data frame and rename the transform data
+  if(transformIntronic==T)
+  {
+    gene_features <- gene_features[,c('trans_start_vec', 'trans_end_vec', 'GC', 'width', 'Type', 'Upper', 'Lower', 'Mid', 'trans_segStart', 'trans_segEnd')]
+    colnames(gene_features) <- c('start', 'end', 'GC', 'width', 'Type', 'Upper', 'Lower', 'Mid', 'segStart', 'segEnd')
+  }
   
   # construct the gene in gplot
   gene_plot <- build_gene(gene_features)
---
+++ R/map_coord_space.R
@@ -0,0 +1,53 @@
+#' Map regions to transformed space
+#' 
+#' Reference a master genomic region file to map original positions to a transformed space
+#' @name mergeRegions
+#' @param master an object of class data frame containing columns start, end, width, type, trans_start, trans_end representing a master genomic region with features from isoforms merged
+#' @param coord an object of class data frame containing columns start and end to map to transformed space
+#' @return an object of class data frame identical to coord but with extra columns for transformed coord
+
+map_coord_space <- function(master, coord)
+{
+  # create empty vectors to dumpt data to
+  trans_start_vec <- vector()
+  trans_end_vec <- vector()
+  
+  #Convert master table into GRanges object to find overlaps in coord
+  master_gr <- GRanges(seqnames=c("chr1"), ranges=IRanges(start=master$start, end=master$end))
+  mcols(master_gr) <- master[,c('Type', 'trans_start', 'trans_end')]
+  
+  # reference the master table with the coordinates and transform the coord to new space
+  for(i in 1:nrow(coord))
+  {
+    coord_gr <- GRanges(seqnames=c("chr1"), ranges=IRanges(start=coord[i,c('start')], end=coord[i,c('end')]))
+    overlap <- subsetByOverlaps(master_gr, coord_gr)
+    
+    # Convert the grange object specfying the overlap coord back to a data frame
+    master_range <- as.data.frame(ranges(overlap))
+    master_meta <- as.data.frame(mcols(overlap))
+    master_overlap <- cbind(master_range, master_meta)
+    master_overlap <- master_overlap[,c('start', 'end', 'width', 'Type', 'trans_start', 'trans_end')]
+    
+    # Convert the grange object containing the coord to transform back to a data frame
+    coord_range <- as.data.frame(ranges(coord_gr))
+    coord_range <- coord_range[,c('start', 'end', 'width')]
+    
+    # Check that there is only 1 row in both coord_range and master_overlap
+    if(nrow(coord_range) != 1 || nrow(master_overlap) != 1)
+    {
+      stop("Expect data frame coord_range and master_overlap in map_coord_space.R to be 1 row")
+    }
+    
+    # Map the original coordinates to the transformed space
+    trans_start <- (coord_range$start - master_overlap$start) + master_overlap$trans_start
+    trans_end <- trans_start + coord_range$width
+    
+    trans_start_vec <- c(trans_start_vec, trans_start)
+    trans_end_vec <- c(trans_end_vec, trans_end)
+    
+  }
+  # Bind the transformed data coordinates to the original data_frame
+  coord <- cbind(coord, trans_start_vec, trans_end_vec)
+  
+  return(coord)
+}
