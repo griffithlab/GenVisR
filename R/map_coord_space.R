2a742a1ff5ae22e3f95870ab9df148d814921f30
Alex H. Wagner <awagner@genome.wustl.edu>
2015-05-14 19:08:29 -0500

    Fixes #15. Fixes #13. Adds ability to scale any element, not just introns.

--- R/map_coord_space.R
+++ R/map_coord_space.R
@@ -1,12 +1,12 @@
 #' Map regions to transformed space
 #' 
 #' Reference a master genomic region file to map original positions to a transformed space
-#' @name mergeRegions
+#' @name map_coord_space
 #' @param master an object of class data frame containing columns start, end, width, type, trans_start, trans_end representing a master genomic region with features from isoforms merged
 #' @param coord an object of class data frame containing columns start and end to map to transformed space
 #' @return an object of class data frame identical to coord but with extra columns for transformed coord
 
-map_coord_space <- function(master, coord, base=exp(1))
+map_coord_space <- function(master, coord)
 {
   #Convert master table into GRanges object to find overlaps in coord
   master_gr <- GRanges(seqnames=c("chr1"), ranges=IRanges(start=master$start, end=master$end))
@@ -21,25 +21,24 @@ map_coord_space <- function(master, coord, base=exp(1))
   master_meta <- as.data.frame(mcols(overlap))
   master_overlap <- cbind(master_range, master_meta)
   master_overlap <- master_overlap[,c('start', 'end', 'width', 'Type', 'trans_start', 'trans_end')]
+  master_overlap$width <- master_overlap$trans_end - master_overlap$trans_start
     
   # Convert the grange object containing the coord to transform back to a data frame
   coord_range <- as.data.frame(ranges(coord_gr))
   coord_range <- coord_range[,c('start', 'end', 'width')]
     
-  # Check that there is only 1 row in both coord_range and master_overlap
-  if(nrow(coord_range) != 1 || nrow(master_overlap) != 1)
+  # Check that there is only 1 row in coord_range
+  if(nrow(coord_range) != 1)
   {
-    stop("Expect data frame coord_range and master_overlap in map_coord_space.R to be 1 row")
+    stop("Expect data frame coord_range map_coord_space.R to be 1 row")
   }
-    
+  
   # Map the original coordinates to the transformed space
-  if( master_overlap$Type != 'Intron' ){
-    trans_start <- (coord_range$start - master_overlap$start) + master_overlap$trans_start
-    trans_end <- trans_start + coord_range$width
-  }else{
-    trans_start <- log(coord_range$start - master_overlap$start, base=base) + master_overlap$trans_start
-    trans_end <- log(coord_range$start - master_overlap$start + coord_range$width, base=base) + master_overlap$trans_start
-  }
+  n <- nrow(master_overlap)
+  fraction.overlap <- (master_overlap[1,]$end - coord_range$start + 1) / (master_overlap[1,]$end - master_overlap[1,]$start + 1)
+  trans_start <- master_overlap[1,]$trans_start + (1 - fraction.overlap) * master_overlap[1,]$width
+  fraction.overlap <- (coord_range$end - master_overlap[n,]$start + 1) / (master_overlap[n,]$end - master_overlap[n,]$start + 1)
+  trans_end <- master_overlap[n,]$trans_start + fraction.overlap * master_overlap[n,]$width
   
   # Add in the new transformed coordinates to the coord object
   coord$trans_start <- trans_start
--- Test_data/Cov_plot_test.R
+++ Test_data/Cov_plot_test.R
@@ -1,8 +1,8 @@
 require("GenomicRanges")
 require('plyr')
 require('ggplot2')
-setwd("/Users/zskidmor/GGgenome/Test_data")
-
+#setwd("/Users/zskidmor/GGgenome/Test_data")
+setwd("/Users/awagner/Workspace/R/GGgenome/Test_data")
 # need a biostrings object for reference
 require(BSgenome.Hsapiens.UCSC.hg19)
 genome <- BSgenome.Hsapiens.UCSC.hg19
@@ -12,8 +12,8 @@ require(TxDb.Hsapiens.UCSC.hg19.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 
 # need Granges object 
-#gr <- GRanges(seqnames=c("chr16"), ranges=IRanges(start=c(67063051), end=c(67134782)), strand=strand(c("+")))
-gr <- GRanges(seqnames=c("chr16"), ranges=IRanges(start=c(67063051), end=c(67063191)), strand=strand(c("+")))
+gr <- GRanges(seqnames=c("chr16"), ranges=IRanges(start=c(67063051), end=c(67134782)), strand=strand(c("+")))
+#gr <- GRanges(seqnames=c("chr16"), ranges=IRanges(start=c(67130782), end=c(67134782)), strand=strand(c("+")))
 
 # need coverage data and extra stuff to make plot work
 cov <- read.delim('CBFB_TCGA_Cov.bed')
@@ -23,7 +23,6 @@ cov <- as.data.frame(cbind(cov[,3], cov[,'cov']))
 colnames(cov) <- c('end', 'cov')
 data <- list("RNA" = cov)
 
-test <- gene_plot(txdb, gr, genome, reduce=F, transformIntronic=T, gene_colour='red')
-#test <- plot_coverage(data, txdb, gr, genome, reduce=F, transformIntronic=T)
-#test <- plot_track('CBFB'=gene, 'Cov'=test_plot, 'Cov'=test_plot, border='green', axis_align='width', width_ratio=c(1,10))
-
+test <- gene_plot(txdb, gr, genome, reduce=T, transform=c('Intron','UTR','CDS'))
+# test <- plot_coverage(data, txdb, gr, genome, reduce=F, transform=c('Intron','UTR','CDS'), cores=8)
+# test <- plot_track('CBFB'=gene, 'Cov'=test_plot, 'Cov'=test_plot, border='green', axis_align='width', width_ratio=c(1,10))
