d11df072510468b8b07b2f69f27be083411e6d88
Zach Skidmore <zskidmor@genome.wustl.edu>
2015-05-21 11:09:04 -0500

    added option to plot or not x axis in transition/transversion plot

--- R/transition_transversion_plot.R
+++ R/transition_transversion_plot.R
@@ -4,13 +4,14 @@
 #' @name transition_transversion_plot
 #' @param x Object of class data frame containing columns 'reference', 'variant'
 #' @param type Object of class character specifying whether to plot the Proportion or Frequency, one of "Prop"
+#' @param label_x_axis boolean specifying wheter to label x axis
 #' @param x_axis_text_angle Integer specifying the angle to labels on x_axis
 #' @param palette Character vector of length 6 specifying colors for trans/tranv type
 #' @return Object of class data frame with indels removed
 #' @import plyr
 #' @export
 
-transition_transversion_plot <- function(x, type='Proportion', x_axis_text_angle=45, palette=c('#7BC374', '#EFCD8D', '#8763A0', '#6677A0', '#EDEE8D', '#EF8D8D'))
+transition_transversion_plot <- function(x, type='Proportion', label_x_axis=TRUE, x_axis_text_angle=45, palette=c('#7BC374', '#EFCD8D', '#8763A0', '#6677A0', '#EDEE8D', '#EF8D8D'))
 {
   # Check that columns are named appropriatley, if not print error
   if(any(grepl('^reference$', colnames(x))) && any(grepl('^variant$', colnames(x))) && any(grepl('^sample$', colnames(x))))
@@ -37,7 +38,7 @@ transition_transversion_plot <- function(x, type='Proportion', x_axis_text_angle
   x$sample <- factor(x$sample, levels=sample_order$sample)
   
   # Build the Transition/Transversion Plot
-  p1 <- build_trans_tranv(x, type=type, x_axis_text_angle=x_axis_text_angle, palette=palette)
+  p1 <- build_trans_tranv(x, type=type, x_axis_text_angle=x_axis_text_angle, palette=palette, label_x_axis=label_x_axis)
   
   return(p1)
 }
