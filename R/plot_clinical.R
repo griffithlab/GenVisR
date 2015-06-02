80985dd6faffea7c256fa30db25ea8923bb72798
Zach Skidmore <zskidmor@genome.wustl.edu>
2015-06-02 17:06:32 -0500

    fixed alignment bug in waterfall plot

--- R/plot_clinical.R
+++ R/plot_clinical.R
@@ -9,7 +9,7 @@
 plot_clinical <- function(x)
 {
   # Define the theme
-  theme <- theme(panel.grid.major = element_blank(), panel.grid.minor=element_blank(), panel.background=element_rect(fill='white', colour='white'), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_blank(), axis.title.x=element_text(size=16), legend.title=element_blank(), axis.title.y=element_blank(), axis.text.y=element_text(size=14, colour='black'), legend.position='right', plot.margin=unit(c(-1,1,.5,.7), 'lines'))
+  theme <- theme(panel.grid.major = element_blank(), panel.grid.minor=element_blank(), panel.background=element_rect(fill='white', colour='white'), axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), axis.text.x=element_blank(), axis.title.x=element_text(size=16), legend.title=element_blank(), axis.title.y=element_blank(), axis.text.y=element_text(size=14, colour='black'), legend.position='right')
   
   
   # Define the main plot
