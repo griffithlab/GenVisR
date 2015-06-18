2df40cb8b947aa01c4275fe51c878b17fd4a317f
Zach Skidmore <zskidmor@genome.wustl.edu>
2015-06-18 16:05:22 -0500

    removed internet dependency for cnSpec, added quality checks, added test data

--- R/cnSpec.qual.R
+++ R/cnSpec.qual.R
@@ -4,10 +4,48 @@
 #' @name cnSpec
 #' @param x object of class data frame containing columns Chromosome, Start, Stop, SegMean, Sample.name
 #' @param y object of class data frame containing option ......
-#' @return ggplot object
-
+#' @return character string specifying input passed quality check
 
 cnSpec.qual <- function(x, y)
 {
+  # Check for internet connection or for a value to y
+  if(is.null(y))
+  {
+    # Check for internet connectivity
+    if(!is.character(getURL("www.google.com")))
+    {
+      stop("Did not detect an internet connection, check internet connectivity or supply a value to y")
+    }
+  }
   
+  if(!is.null(y))
+  {
+    # Check that y is a data frame
+    if(!is.data.frame(y))
+    {
+      message("y is not a data frame, attempting to coerce")
+      y <- as.data.frame(y)
+    }
+    
+    # Check column names of y
+    if(!all(c('chromosome', 'start', 'end') %in% colnames(y)))
+    {
+      stop("Did not detect correct columns in y, missing one of chromosome, start, end")
+    }
+  }
+  
+  # Check that x is a data frame
+  if(!is.data.frame(x))
+  {
+    message("x is not a data frame, attempting to coerce")
+    x <- as.data.frame(x)
+  }
+  
+  # Check column names of x
+  if(!all(c('chromosome', 'start', 'end', 'segmean', 'sample') %in% colnames(x)))
+  {
+    stop("Did not detect correct columns in x, missing one of chromosome, start, end, segmean, sample")
+  }
+  
+  return("Input data passed basic quality check")
 }
