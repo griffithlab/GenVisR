5be0f67eadcf1ddfe6b47eccadc0fbb05b9fe73d
Zach Skidmore <zskidmor@genome.wustl.edu>
2015-07-10 21:47:38 -0500

    fixed bug related to calling format_ideogram with additional columns, finished rough first draft of documentation

--- R/GenVisR.R
+++ R/GenVisR.R
@@ -16,14 +16,48 @@ NULL
 #' @format a data frame with 2773 observations and 55 variables
 NULL
 
+#' cytogenetic band information for select genomes
+#' 
+#' A data set containing cytogenetic band information for all chromosomes in the following genomes "hg38", "hg19", "mm10", "mm9", "rn5", obtained from the UCSC sql database at genome-mysql.cse.ucsc.edu
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name cytoGeno
+#' @usage data(cytoGeno)
+#' @format a data frame with 3207 observations and 6 variables
+NULL
+
+#' hg19 chromosome boundaries
+#' 
+#' A data set containg chromosome boundaries corresponding to hg19 in base pairs
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name hg19chr
+#' @usage data(hg19chr)
+#' @format a data frame with 24 observations and 3 variables
+NULL
+
+
+#' CN data for Luc2
+#' 
+#' Downsampled copy number calls for Luc2 chromosome 14 originating from cnv-hmm "Genomic Landscape of Non-Small Cell Lung Cancer in Smokers and Never-Smokers" PMID:22980976
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name Luc2CNraw
+#' @usage data(Luc2CNraw)
+#' @format a data frame with 10000 observations and 5 variables
+NULL
+
 #' Truncated CN segments
 #' 
 #' A data set in long format containing Copy Number segments for 4 samples corresponding to "lung cancer" from "Genomic Landscape of Non-Small Cell Lung Cancer in Smokers and Never-Smokers" PMID:22980976
 #' 
 #' @docType data
 #' @keywords datasets
-#' @name LucCN
-#' @usage data(LucCN)
+#' @name LucCNseg
+#' @usage data(LucCNseg)
 #' @format a data frame with 3336 observations and 6 variables
 NULL
 
