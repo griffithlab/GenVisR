9b9bd2314d498966e24202b0161ebfcc1c1f7035
Zach Skidmore <zskidmor@genome.wustl.edu>
2015-12-01 11:38:43 -0600

    additional fixes and cleanup of LOH code

--- R/lohView_windowPosition.R
+++ R/lohView_windowPosition.R
@@ -8,14 +8,13 @@
 #' @param window_size integer with the size of the sliding window (bp) to be 
 #' applied
 #' @return list containing window start/stop positions for each chromosome 
-#' from each sample
-#' to perform LOH calculations
-#' @import dplyr
-#' @export
+#' from each sample to perform LOH calculations
 
 
-lohView_windowPosition <- function(out, step, window_size) {
-    window <- lapply(out, function(x) {
+lohView_windowPosition <- function(out, step, window_size)
+{
+    window <- lapply(out, function(x) 
+    {
         min <- integer()
         max <- integer()
         window_stop_1 <- integer()
@@ -27,16 +26,20 @@ lohView_windowPosition <- function(out, step, window_size) {
         window_data <- data.frame()
         
         num <- trunc(window_num)
-        for (w in 1:num) {
+        
+        for (w in 1:num)
+        {
             window_data[w,1] <- as.integer(min + (step*(w-1)))
             window_data[w,2] <- as.integer(window_stop_1 + (step*(w-1)))
         }
+        
         colnames(window_data) <- c("window_start", "window_stop")
-        window_final <- dplyr::filter(window_data, 
-                                      max > window_data$window_stop)
+        window_final <- window_data[window_data$window_stop < max,]
         window_final[nrow(window_final), 2] <- max
+        
         return(window_final)
     })
+    
     return(window)
 }
     
