456602c0e0240e22853b9c25bbef3001936c2275
Katie Campbell <kcampbel@Katies-MacBook-Pro.local>
2015-12-16 15:09:57 -0600

    Woooohoo successfully added test cases to compIdent. Feelin pretty compIdent about it

--- R/compIdent.R
+++ R/compIdent.R
@@ -11,14 +11,20 @@
 #' #TODO
 #' @export
 
-compIdent <- function(x, genome)
+compIdent <- function(x, genome, debug=FALSE)
 {
-    # Grab the bam files and samples from the data frame
-    bams <- as.character(x$bamfile)
-    samplenames <- as.character(x$sample_name)
+    if(isTRUE(debug)){
+        bams <- list(normal=HCC1395_N, tumor=HCC1395_T)
+        samplenames <- c('normal','tumor')
+        count_tables <- lapply(bams, compIdent_bamRcnt, genome=genome, debug=TRUE)
+    } else{ 
+        # Grab the bam files and samples from the data frame
+        bams <- as.character(x$bamfile)
+        samplenames <- as.character(x$sample_name)
 
-    # Readcount the supplied bam files
-    count_tables <- lapply(bams, compIdent_bamRcnt, genome=genome)
+        # Readcount the supplied bam files
+        count_tables <- lapply(bams, compIdent_bamRcnt, genome=genome)
+    }
 
     # make an sample identity plot
     plot <- compIdent_buildMain(count_tables,samplenames)
