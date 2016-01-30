fc51b2bc4acdb9dbf2633fc89710e14619f1850f
Zachary Skidmore <zskidmor@genome.wustl.edu>
2016-01-30 22:20:10 +0000

    fixed reference to GenomicRanges::unlist
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenVisR@113108 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/geneViz_cdsFromTXID.R
+++ R/geneViz_cdsFromTXID.R
@@ -6,7 +6,7 @@
 #' @param txid A list of TXIDs
 #' @return Object of class Granges
 #' @importFrom GenomicRanges GRanges
-#' @importFrom GenomicRanges unlist
+#' @importFrom BiocGenerics unlist
 #' @importFrom IRanges IRanges
 #' @importFrom AnnotationDbi select
 #' @importFrom GenomicRanges GRangesList
@@ -16,31 +16,31 @@ geneViz_cdsFromTXID <- function(txdb, txid)
     # Declare Anonymous functions
     f <- function(r)
     {
-        g <- GenomicRanges::GRanges(seqnames = GenomicRanges::unlist(r['CDSCHROM']),
-                     ranges=IRanges::IRanges(start = as.numeric(GenomicRanges::unlist(r['CDSSTART'])), end = as.numeric(GenomicRanges::unlist(r['CDSEND']))),
-                     strand=GenomicRanges::unlist(r['CDSSTRAND']),
-                     txname=GenomicRanges::unlist(r['TXNAME']),
-                     exonrank=GenomicRanges::unlist(r['EXONRANK']))
+        g <- GenomicRanges::GRanges(seqnames = BiocGenerics::unlist(r['CDSCHROM']),
+                     ranges=IRanges::IRanges(start = as.numeric(BiocGenerics::unlist(r['CDSSTART'])), end = as.numeric(BiocGenerics::unlist(r['CDSEND']))),
+                     strand=BiocGenerics::unlist(r['CDSSTRAND']),
+                     txname=BiocGenerics::unlist(r['TXNAME']),
+                     exonrank=BiocGenerics::unlist(r['EXONRANK']))
         return(g)
-    }    
-    
+    }
+
     r <- AnnotationDbi::select(txdb, as.character(txid), c("CDSSTART","CDSEND",
                                                            "CDSCHROM",
                                                            "CDSSTRAND","CDSID",
                                                            "TXID", "EXONRANK",
                                                            "TXNAME"), "TXID")
-    
+
     idx <- as.vector(!is.na(r['CDSSTART']))
     r <- r[idx,]
-    
+
     if (!any(idx))
     {
         return(NA)
     }
-    
+
     r <- split(r, r['TXID'])
 
-    cds <- GenomicRanges::GRangesList(GenomicRanges::unlist(lapply(r, f)))
-    
+    cds <- GenomicRanges::GRangesList(BiocGenerics::unlist(lapply(r, f)))
+
     return(cds)
 }
