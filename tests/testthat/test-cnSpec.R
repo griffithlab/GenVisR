2878539faaa5f85d79bf6a24f30c48e8e9f8c0fe
Zachary Skidmore <zskidmor@genome.wustl.edu>
2016-02-29 22:31:57 +0000

    altered test cases
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenVisR@114109 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test-cnSpec.R
+++ tests/testthat/test-cnSpec.R
@@ -1,7 +1,7 @@
 test_that("cnSpec plot data is formated as expected", {
     x <- data.frame(chromosome=c('chr1', 'chr1'), start=c(1000, 5000), end=c(5000, 10000), segmean=c(0, 4), sample=c('samp1', 'samp2'))
     y <- data.frame(chromosome=c('chr1', 'chr1'), start=c(0), end=c(10000))
-    out <- cnSpec(x, y, dataOut=TRUE)
+    out <- cnSpec(x, y, out="data")
     expect <- data.frame(chromosome=as.factor(rep(1, 12)),
                          start=rep(c(1000, 5000, 0, 0, 10000, 10000), 2),
                          end=rep(c(5000, 10000, 0, 0, 10000, 10000), 2),
--- tests/testthat/test-waterfall.R
+++ tests/testthat/test-waterfall.R
@@ -1,11 +1,11 @@
 test_that("waterfall levels data correctly and as such data aligns", {
-    out <- waterfall(brcaMAF, plotGenes=c("PIK3CA", "TP53"), dataOut=T)
+    out <- waterfall(brcaMAF, plotGenes=c("PIK3CA", "TP53"), out="data")
     expect_equal(levels(out[["main"]]$sample), levels(out[["mutation_count"]]$sample))    
 })
 
 test_that("waterfall correctly counts mutations in a sample", {
     # Obtain output
-    out <- waterfall(brcaMAF, plotGenes=c("PIK3CA", "TP53"), dataOut=T)
+    out <- waterfall(brcaMAF, plotGenes=c("PIK3CA", "TP53"), out="data")
     out_A <- out$mutation[out$mutation_count$sample == "TCGA-A1-A0SI-01A-11D-A142-09" &
                               out$mutation_count$trv_type == "Synonymous",]
     out_B <- out$mutation[out$mutation_count$sample == "TCGA-A1-A0SI-01A-11D-A142-09" &
