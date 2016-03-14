dd6d4699ddfdbfb71aa72284b6073b21120d1111
Zachary Skidmore <zskidmor@genome.wustl.edu>
2016-03-14 18:38:07 +0000

    bug fix for prior commit
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GenVisR@114741 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/waterfall_NA2gene.R
+++ R/waterfall_NA2gene.R
@@ -11,8 +11,14 @@ waterfall_NA2gene <- function(x)
 {
     # Get The gene with the most Mutations and add the NA samples to that gene
     # (ensures that the NAs are added in as gene with most mutations will always
-    # be plotted)
+    # be plotted) i.e. makes sure that samples are plotted
+    
+    # find top gene
     top_gene <- stats::na.omit(rev(x$gene))[1]
+    
+    # set the trv_type to NA if gene is NA (makes sure that wile a sample is plotted the cell is empty)
+    x$trv_type <- replace(x$trv_type, is.na(x$gene), NA)
     x$gene <- replace(x$gene, is.na(x$gene), top_gene)
+    
     return(x)
 }
