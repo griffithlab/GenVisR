844838ecc4b968a37093e7c9c502c8db9c3e390f
Zach Skidmore <zskidmor@genome.wustl.edu>
2016-03-30 10:38:55 -0500

    enhance cnSpec, adde citation

--- tests/testthat/test-cnSpec.R
+++ tests/testthat/test-cnSpec.R
@@ -2,10 +2,16 @@ test_that("cnSpec plot data is formated as expected", {
     x <- data.frame(chromosome=c('chr1', 'chr1'), start=c(1000, 5000), end=c(5000, 10000), segmean=c(0, 4), sample=c('samp1', 'samp2'))
     y <- data.frame(chromosome=c('chr1', 'chr1'), start=c(0), end=c(10000))
     out <- cnSpec(x, y, out="data")
-    expect <- data.frame(chromosome=as.factor(rep(1, 12)),
-                         start=rep(c(1000, 5000, 0, 0, 10000, 10000), 2),
-                         end=rep(c(5000, 10000, 0, 0, 10000, 10000), 2),
-                         sample=c(rep('samp1', 6), rep('samp2', 6)),
-                         cn=c(0, rep(NA, 6), 4, rep(NA, 4)))
-    expect_equal(out[[1]], expect)
+    expect_a <- data.frame(chromosome=as.factor(rep(1, 2)),
+                         start=c(1000, 5000),
+                         end=c(5000, 10000),
+                         sample=c('samp1', 'samp2'),
+                         cn=c(0, 4))
+    expect_b <- data.frame(chromosome=as.factor(rep(1, 4)),
+                           start=rep(c(0, 10000), 2),
+                           end=rep(c(0, 10000), 2),
+                           sample=c('samp1', 'samp1', 'samp2', 'samp2'))
+    expect_equivalent(out[[1]], expect_a)
+    expect_equivalent(out[[2]], expect_b)
+    
 })
