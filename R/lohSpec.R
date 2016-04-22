05cff5211c1e20ef94064bde50df7c1478a6af47
Zach Skidmore <zskidmor@genome.wustl.edu>
2016-04-22 15:01:09 -0500

    changed vaf expectation to be from 0-1 instead of 0-100, fixes #238

--- R/lohSpec.R
+++ R/lohSpec.R
@@ -75,7 +75,7 @@
 
 lohSpec <- function(x=NULL, path=NULL, fileExt=NULL, y=NULL, genome='hg19',
                     gender=NULL, step=1000000, window_size=2500000, 
-                    normal=50, gradient_midpoint=20, gradient_low="#ffffff",
+                    normal=.50, gradient_midpoint=.20, gradient_low="#ffffff",
                     gradient_mid="#b2b2ff", gradient_high="#000000",
                     theme_layer=NULL, method="slide", out="plot")
 {
