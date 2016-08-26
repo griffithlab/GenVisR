8c648b3a022a3d5f970716b645198e7bbc7f0be7
Zach Skidmore <zskidmor@genome.wustl.edu>
2016-08-26 17:32:40 -0500

    added skeleton for waterfall class

---
+++ R/Waterfall-class.R
@@ -0,0 +1,43 @@
+#' Class Waterfall
+#' 
+#' An S4 class for the waterfall plot object
+#' @name Waterfall-class
+#' @rdname Waterfall-class
+#' @slot plotA gtable object for the top sub-plot.
+#' @slot plotB gtable object for the left sub-plot.
+#' @slot plotC gtable object for the main plot.
+#' @slot plotD gtable object for the bottom sub-plot.
+#' @slot Grob gtable object for the arranged plot.
+#' @slot Data list of data.table objects storing the plotted data.
+#' @exportClass Waterfall
+#' @import methods
+setClass("Waterfall",
+         representation=representation(plotA="gtable",
+                                       plotB="gtable",
+                                       plotC="gtable",
+                                       plotD="gtable",
+                                       Grob="gtable",
+                                       Data="list")
+         )
+
+#' Initalizer method for the Waterfall class
+#' 
+#' @name Waterfall
+#' @rdname Waterfall-class
+setMethod(f="initalize",
+          signature="Waterfall",
+          definition=function(.Object, input){
+              
+          })
+
+#' Constructor for the Waterfall class.
+#' 
+#' @name Waterfall
+#' @rdname Waterfall-class
+#' @param input MutationAnnotationFormat class holding genomic information.
+#' @export
+Waterfall <- function(input){
+    cat("!!!!! Waterfall~Constructor !!!!!\n")
+    new("Waterfall", input=input)
+}
+
