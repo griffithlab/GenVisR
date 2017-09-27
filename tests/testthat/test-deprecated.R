703af2cda75b0a101001fb70b6692cc92b879a38
Zach Skidmore <zskidmor@genome.wustl.edu>
2017-09-27 10:40:10 -0500

    moved unit tests for deprecated functions

--- tests/testthat/test-deprecated.R
+++ tests/testthat/test-deprecated.R
@@ -364,3 +364,203 @@ test_that("waterfall_hierarchyTRV recognizes an invalid mutation type", {
     file_type <- 'MAF'
     expect_error(waterfall_hierarchyTRV(x, file_type, variant_class_order), "invalid mutation type")
 })
+
+test_that("TvTi_rmMnuc correctly identifies and removes multi-nucleotide codes", {
+    x <- data.frame("reference"=c("AA", "T"), "variant"=c("AT", "C"))
+    expect_equal(nrow(suppressWarnings(TvTi_rmMnuc(x))), 1)
+    
+    x <- data.frame("reference"=c("gg", "t"), "variant"=c("cc", "g"))
+    expect_equal(nrow(suppressWarnings(TvTi_rmMnuc(x))), 1)
+})
+
+test_that("TvTi_rmMnuc warns if it finds multi-nucleotide codes", {
+    x <- data.frame("reference"=c("AA", "T"), "variant"=c("AT", "C"))
+    
+    expect_warning(TvTi_rmMnuc(x), "multi-nucleotides present")   
+})
+
+test_that("TvTi_calcTransTranvFreq adds in trans/tranv missing from samples", {
+    x <- data.frame(reference=c("A", "G"),
+                    variant=c("T", "C"),
+                    sample=c("samp1", "samp2"),
+                    trans_tranv=c("A->T or T->A (TV)", "A->G or T->C (TI)"))
+    expect_equal(nrow(TvTi_calcTransTranvFreq(x)), 12)
+})
+
+test_that("TvTi_calcTransTranvFreq correctly calculates the proportions of TR/TV", {
+    x <- data.frame(reference=c("A", "G"),
+                    variant=c("T", "C"),
+                    sample=c("samp1", "samp1"),
+                    trans_tranv=c("A->T or T->A (TV)", "A->G or T->C (TI)")) 
+    out <- TvTi_calcTransTranvFreq(x)
+    expect_equal(out[out$trans_tranv == "A->G or T->C (TI)",]$Prop, .5)
+    
+    x <- data.frame(reference=c("A", "G"),
+                    variant=c("T", "C"),
+                    sample=c("samp1", "samp2"),
+                    trans_tranv=c("A->T or T->A (TV)", "A->G or T->C (TI)"))
+    out <- TvTi_calcTransTranvFreq(x)
+    expect_equal(sum(out$Prop), 2)
+})
+
+test_that("TvTi_calcTransTranvFreq correctly calculates the frequency of Tv/Ti", {
+    x <- data.frame(reference=c("A", "G"),
+                    variant=c("T", "C"),
+                    sample=c("samp1", "samp1"),
+                    trans_tranv=c("A->T or T->A (TV)", "A->G or T->C (TI)")) 
+    out <- TvTi_calcTransTranvFreq(x)
+    expect_equal(out[out$trans_tranv == "A->G or T->C (TI)",]$Freq, 1)
+    expect_equal(out[out$trans_tranv == "A->T or T->A (TV)",]$Freq, 1)
+    
+    x <- data.frame(reference=c("A", "G"),
+                    variant=c("T", "C"),
+                    sample=c("samp1", "samp2"),
+                    trans_tranv=c("A->T or T->A (TV)", "A->G or T->C (TI)"))
+    out <- TvTi_calcTransTranvFreq(x)
+    expect_equal(sum(out$Freq), 2)
+})
+
+test_that("TvTi_convMaf Detects Homozygous events and only counts them once", {
+    x <- data.frame(Tumor_Sample_Barcode=c("A"), Reference_Allele="C", Tumor_Seq_Allele1=c("T"), Tumor_Seq_Allele2=c("T"))
+    expect <- data.frame(sample=c("A"), reference=c("C"), variant=c("T"))
+    expect_equal(TvTi_convMaf(x), expect)
+})
+
+test_that("TvTi_convMaf eliminates calls where the reference allele equals the variant allele", {
+    x <- data.frame(Tumor_Sample_Barcode=c("A"), Reference_Allele="C", Tumor_Seq_Allele1=c("C"), Tumor_Seq_Allele2=c("C"))
+    expect_equal(nrow(TvTi_convMaf(x)), 0)
+    
+    x <- data.frame(Tumor_Sample_Barcode=c("A"), Reference_Allele="C", Tumor_Seq_Allele1=c("T"), Tumor_Seq_Allele2=c("C"))
+    expect_equal(nrow(TvTi_convMaf(x)), 1)
+    
+    x <- data.frame(Tumor_Sample_Barcode=c("A"), Reference_Allele="C", Tumor_Seq_Allele1=c("C"), Tumor_Seq_Allele2=c("T"))
+    expect_equal(nrow(TvTi_convMaf(x)), 1)
+})
+
+test_that("TvTi_qual correctly identifies invalid input to the fileType parameter", {
+    x <- data.frame(sample=c("a", "a"), reference=c("a", "a"), variant=c("c", "c"))
+    y <- NULL
+    file_type="INCORRECT"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "input to paramter fileType")
+})
+
+test_that("TvTi_qual correctly identifies if input to x is not of proper class", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    x <- as.matrix(x)
+    y <- NULL
+    file_type="MGI"
+    expect_warning(TvTi_qual(x, y=y, file_type=file_type), "not an object of class data frame")
+})
+
+test_that("TvTi_qual correctly identifies if duplicate rows are present in input to x", {
+    x <- data.frame(sample=c("a", "a"), reference=c("a", "a"), variant=c("c", "c"))
+    y <- NULL
+    file_type="MGI"
+    expect_warning(TvTi_qual(x, y=y, file_type=file_type), "Detected duplicate rows")
+})
+
+test_that("TvTi_qual correctly identifies if input to y is not of proper class", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- c("A->C or T->G (TV)"=1/6, "A->G or T->C (TI)"=1/6, "A->T or T->A (TV)"=1/6, "G->A or C->T (TI)"=1/6, "G->C or C->G (TV)"=1/6, "G->T or C->A (TV)"=1/6)
+    y <- as.matrix(y)
+    file_type <- "MGI"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "not an object of class data frame")
+})
+
+test_that("TvTi_qual Checks for proper column names if input to y is a data frame", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- data.frame(Prop=rep(1/6, 6), incorrect=rep("A->C or T->G (TV)", 6))
+    file_type <- "MGI"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "Did not detect correct column names")
+})
+
+test_that("TvTi_qual checks that proportion values are of numeric type", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- c("A->C or T->G (TV)"="1/6", "A->G or T->C (TI)"="1/6", "A->T or T->A (TV)"="1/6", "G->A or C->T (TI)"="1/6", "G->C or C->G (TV)"="1/6", "G->T or C->A (TV)"="1/6")
+    file_type <- "MGI"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "not of type double or numeric")
+})
+
+test_that("TvTi_qual recognizes if input to y is not of proper class", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- c("A->C or T->G (TV)"="1/6", "A->G or T->C (TI)"="1/6", "A->T or T->A (TV)"="1/6", "G->A or C->T (TI)"="1/6", "G->C or C->G (TV)"="1/6", "G->T or C->A (TV)"="1/6")
+    y <- as.matrix(y)
+    file_type <- "MGI"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "input to y is not")
+})
+
+test_that("TvTi_qual detects if proper column names are not supplied to x", {
+    x <- data.frame(incorrect=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- data.frame(Prop=rep(1/6, 6), trans_tranv=rep("A->C or T->G (TV)", 6))
+    file_type <- "MGI"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "not find all columns")
+    
+    x <- data.frame(Tumor_Sample_Barcode=c("a", "b"), incorrect=c("a", "a"), Tumor_Seq_Allele1=c("a", "a"), Tumor_Seq_Allele2=c("a", "a"))
+    file_type <- "MAF"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "not find all columns")
+})
+
+test_that("TvTi_qual detects unexpected nucleotide codes in input supplied to x", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "I"), variant=c("g", "c"))
+    y <- data.frame(Prop=rep(1/6, 6), trans_tranv=rep("A->C or T->G", 6))
+    file_type <- "MGI"   
+    
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "Unrecognized Base")
+    
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "I"), variant=c("I", "c"))
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "Unrecognized Base")
+})
+
+test_that("TvTi_qual checks for proper transition/transversion levels in input to y", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- data.frame(Prop=rep(1/6, 6), trans_tranv=rep("A->C or T->G (TV)", 6))
+    file_type <- "MGI"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "all combinations of transitions/transversions")
+})
+
+test_that("TvTi_qual checks that proportions supplied in y sum to one", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- c("A->C or T->G (TV)"=1/5, "A->G or T->C (TI)"=1/6, "A->T or T->A (TV)"=1/6, "G->A or C->T (TI)"=1/6, "G->C or C->G (TV)"=1/6, "G->T or C->A (TV)"=1/6)
+    file_type <- "MGI"
+    expect_error(TvTi_qual(x, y=y, file_type=file_type), "should equal 1")
+})
+
+test_that("TvTi_qual identifies if argument supplied to z is not a data frame", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- NULL
+    z <- data.frame("sample"=c("x"), "variable"=c("y"), "value"=c("Z"))
+    z <- as.matrix(z)
+    file_type <- "MGI"  
+    expect_error(TvTi_qual(x, y=y, z=z, file_type=file_type), "Did not detect")
+})
+
+test_that("TvTi_qual identifies if argument supplied to z contains incorrect column names", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- NULL
+    z <- data.frame("INCORRECT"=c("x"), "variable"=c("y"), "value"=c("Z"))
+    file_type <- "MGI"  
+    expect_error(TvTi_qual(x, y=y, z=z, file_type=file_type), "Did not detect")    
+})
+
+test_that("TvTi_qual identifies if samples supplied to argument x and z differ", {
+    x <- data.frame(sample=c("a", "b"), reference=c("a", "t"), variant=c("g", "c"))
+    y <- NULL
+    z <- data.frame("sample"=c("x"), "variable"=c("y"), "value"=c("Z"))
+    file_type <- "MGI"  
+    expect_warning(TvTi_qual(x, y=y, z=z, file_type=file_type), "Found a sample") 
+})
+
+test_that("TvTi_rmIndel correctly identifies and removes indels", {
+    x <- data.frame("reference"=c('-', 'A'), "variant"=c('A', 'A'))    
+    expect_equal(nrow(suppressWarnings(TvTi_rmIndel(x))), 1)
+    
+    x <- data.frame("reference"=c('A', 'A'), "variant"=c('0', 'A'))    
+    expect_equal(nrow(suppressWarnings(TvTi_rmIndel(x))), 1)    
+})
+
+test_that("TvTi_rmIndel correctly warns if indels are detected", {
+    x <- data.frame("reference"=c('-', 'A'), "variant"=c('A', 'A'))
+    
+    expect_message(TvTi_rmIndel(x), "indels present")
+})
+
