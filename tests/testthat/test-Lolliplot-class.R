b871bd85fd8fc4e3d74154e6275d9644e058a233
Zachary Skidmore <zlskidmore@gmail.com>
2022-01-31 15:27:28 -0600

    fix unit test

--- tests/testthat/test-Lolliplot-class.R
+++ tests/testthat/test-Lolliplot-class.R
@@ -520,7 +520,7 @@ if(biomart_success){
 test_that("setDomainHeights domains are nested correctly", {
 
     skip_if_not(biomart_success, "mart recieved try-error")
-    expected <- 7
+    expected <- 9
     
     actual <- setDomainHeights.mode1.out[setDomainHeights.mode1.out$interproShortDesc == "PI3/4_kinase_cat_sf",]$nest
     expect_equivalent(expected, actual)
